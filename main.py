97b44c80c016e107d1faf638cb282eea5207e28c
soup-bowl <code@soupbowl.io>
2024-01-20 17:26:59 +0000

    use main endpoint, no cache

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ import random
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/random-image")
 async def get_random_image():
     images_directory = "/images"
     try:
@@ -21,6 +17,8 @@ async def get_random_image():
         random_image = random.choice(images)
         image_path = os.path.join(images_directory, random_image)
 
-        return FileResponse(image_path)
+        response = FileResponse(image_path)
+        response.headers["Cache-Control"] = "no-store"
+        return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
